88a39c53af0239ba28a9ea9fc171296b3ab214a2
YasiruNilupul <wickramageyn.22@UOM.lk>
2023-03-23 22:00:50 +0530

    Update Largest prime factor.py

--- Largest prime factor.py
+++ Largest prime factor.py
@@ -1,15 +1,12 @@
-num=100
-list=[]
-k=0
-for i in range(1,num+1):
-    if num%i==0:
-        k=0
-        for j in range(1,i+1):
-          #if i%2==0:
-              #break
-          if i %j==0:
-              k+=1
-        if k==2:
-            list.append(i)
-           
-print(max(list))
+num = 600851475143
+largest_prime_factor = 1
+i = 2
+
+while i <= num:
+    if num % i == 0:
+        largest_prime_factor = i
+        num = num / i
+    else:
+        i += 1
+
+print(largest_prime_factor)
